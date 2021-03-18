c7c790c1884e69838d101413c102cda916e55e2c
Алексей Кочнев <alexcochnev@inbox.ru>
2021-03-19 02:28:37 +0300

    Последовательная отправка запросов, дубровка в конце

--- scripts/main.js
+++ scripts/main.js
@@ -12,33 +12,24 @@ function rowInsert(json, tbody) {
     });
 }
 
-async function requestDubrovka() {
-    let date = document.getElementById('dateMin').value;
-    let proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-        targetUrl = 'http://squashschedule.herokuapp.com/dubrovka_day/?date=' + date;
-    let response = await fetch(proxyUrl + targetUrl);
+async function requestDubrovka(date) {
+    let response = await fetch('http://squashschedule.herokuapp.com/dubrovka_day/?date=' + date);
     let jsonResp = await response.json();
 
     let tbody = d.getElementById('dubrovka').getElementsByTagName('TBODY')[0];
     rowInsert(jsonResp,tbody)
 }
 
-async function requestShabolovka() {
-    let date = document.getElementById('dateMin').value;
-    let proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-        targetUrl = 'http://squashschedule.herokuapp.com/shabolovka_day/?date=' + date;
-    let response = await fetch(proxyUrl + targetUrl);
+async function requestShabolovka(date) {
+    let response = await fetch('http://squashschedule.herokuapp.com/shabolovka_day/?date=' + date);
     let jsonResp = await response.json();
 
     let tbody = d.getElementById('shabolovka').getElementsByTagName('TBODY')[0];
     rowInsert(jsonResp,tbody)
 }
 
-async function requestLeninka() {
-    let date = document.getElementById('dateMin').value;
-    let proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-        targetUrl = 'http://squashschedule.herokuapp.com/leninka_days/?date=' + date;
-    let response = await fetch(proxyUrl + targetUrl);
+async function requestLeninka(date) {
+    let response = await fetch('http://squashschedule.herokuapp.com/leninka_days/?date=' + date);
     let jsonResp = await response.json();
 
     let tbody = d.getElementById('leninka').getElementsByTagName('TBODY')[0];
@@ -47,17 +38,39 @@ async function requestLeninka() {
     }
 }
 
-function apiRequest() {
+async function apiRequest() {
+    let dateMax = new Date(d.getElementById('dateMax').value);
     let checkDubrovka = d.getElementById('checkDubrovka');
     let checkShabolovka = d.getElementById('checkShabolovka');
     let checkLeninka = d.getElementById('checkLeninka');
-    if (checkDubrovka.checked) {
-        requestDubrovka()
-    }
-    if (checkShabolovka.checked) {
-        requestShabolovka()
-    }
+
     if (checkLeninka.checked) {
-        requestLeninka()
+        let dateMin3 = new Date(d.getElementById('dateMin').value);
+        let dateRequest = dateMin3.toISOString().substr(0,10)
+        while (dateMin3<dateMax) {
+            dateMin3.setDate(dateMin3.getDate()+1)
+            dateRequest += ',' + dateMin3.toISOString().substr(0,10)
+        }
+        await requestLeninka(dateRequest)
+    }
+
+    if (checkShabolovka.checked) {
+        let dateMin2 = new Date(d.getElementById('dateMin').value);
+        await requestShabolovka(dateMin2.toISOString().substr(0,10));
+        dateMin2.setDate(dateMin2.getDate()+1)
+        while (dateMin2<=dateMax) {
+            await requestShabolovka(dateMin2.toISOString().substr(0,10));
+            dateMin2.setDate(dateMin2.getDate()+1)
+        }
+    }
+
+    if (checkDubrovka.checked) {
+        let dateMin1 = new Date(d.getElementById('dateMin').value);
+        await requestDubrovka(dateMin1.toISOString().substr(0,10));
+        dateMin1.setDate(dateMin1.getDate()+1);
+        while (dateMin1<=dateMax) {
+            await requestDubrovka(dateMin1.toISOString().substr(0,10));
+            dateMin1.setDate(dateMin1.getDate()+1)
+        }
     }
 }
